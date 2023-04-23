3424df229de4eecbc41a0604b2796096a04159a2
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-23 18:18:31 +0200

    Spinner and TV Shows done

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+// Display 20 most popular movies
+
 async function displayPopularMovies() {
   const { results } = await fetchApiData('/movie/popular');
   console.log(results);
@@ -31,15 +33,49 @@ async function displayPopularMovies() {
   });
 }
 
+// Display 20 most popular TV shows
+
+async function displayPopularShows() {
+  const { results } = await fetchApiData('/tv/popular');
+  console.log(results);
+  results.forEach((show) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <a href="movie-details.html?id=${show.id}">
+      ${
+        show.poster_path
+          ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" 
+           class="card-img-top"
+           alt="${show.name}" />`
+          : `<img src="../images/no-image.jpg" 
+           class="card-img-top"
+           alt="${show.name}" />`
+      }
+      </a>
+        <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+            <small class="text-muted">Air date: ${show.first_air_date}</small>
+          </p>
+        </div>`;
+
+    document.querySelector('#popular-shows').appendChild(div);
+  });
+}
+
 // Fetch data from TMDB API
 async function fetchApiData(endpoint) {
   const API_KEY = '071f2c75b917ebd7880005c5e28c7563';
   const API_URL = 'https://api.themoviedb.org/3';
 
+  showSpinner();
+
   const response = await fetch(
     `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
   );
   const data = await response.json();
+  hideSpinner();
   return data;
 }
 
@@ -53,6 +89,14 @@ function highlightActiveLink() {
   });
 }
 
+function showSpinner() {
+  document.querySelector('.spinner').classList.add('show');
+}
+
+function hideSpinner() {
+  document.querySelector('.spinner').classList.remove('show');
+}
+
 // Init app
 function init() {
   switch (global.currentPage) {
@@ -61,7 +105,7 @@ function init() {
       displayPopularMovies();
       break;
     case '/shows.html':
-      console.log('Shows');
+      displayPopularShows();
       break;
     case '/movie-details.html':
       console.log('Movie Details');
