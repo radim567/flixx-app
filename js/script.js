7a351e67e1dc221848160e164d4ef9c5e2c6dea5
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-24 00:07:04 +0200

    Detail Page almost done

--- js/script.js
+++ js/script.js
@@ -72,47 +72,57 @@ async function displayMovieDetails() {
   div.innerHTML = `
       <div class="details-top">
           <div>
-            <img
-              src="images/no-image.jpg"
-              class="card-img-top"
-              alt="Movie Title"
-            />
+              ${
+                movie.poster_path
+                  ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+           class="card-img-top"
+           alt="${movie.title}" />`
+                  : `<img src="../images/no-image.jpg" 
+           class="card-img-top"
+           alt="${movie.title}" />`
+              }
           </div>
           <div>
-            <h2>Movie Title</h2>
+            <h2>${movie.title}</h2>
             <p>
               <i class="fas fa-star text-primary"></i>
-              8 / 10
+              ${movie.vote_average.toFixed(1)} / 10
             </p>
-            <p class="text-muted">Release Date: XX/XX/XXXX</p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-              atque molestiae error debitis provident dolore hic odit, impedit
-              sint, voluptatum consectetur assumenda expedita perferendis
-              obcaecati veritatis voluptatibus. Voluptatum repellat suscipit,
-              quae molestiae cupiditate modi libero dolorem commodi obcaecati!
-              Ratione quia corporis recusandae delectus perspiciatis consequatur
-              ipsam. Cumque omnis ad recusandae.
+              ${movie.overview}
             </p>
             <h5>Genres</h5>
             <ul class="list-group">
-              <li>Genre 1</li>
-              <li>Genre 2</li>
-              <li>Genre 3</li>
+              ${movie.genres
+                .map(
+                  (genre) => `<li class="list-group-item">${genre.name}</li>`
+                )
+                .join('')}
             </ul>
-            <a href="#" target="_blank" class="btn">Visit Movie Homepage</a>
+            <a href="${
+              movie.homepage
+            }" target="_blank" class="btn">Visit Movie Homepage</a>
           </div>
         </div>
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $1,000,000</li>
-            <li><span class="text-secondary">Revenue:</span> $2,000,000</li>
-            <li><span class="text-secondary">Runtime:</span> 90 minutes</li>
-            <li><span class="text-secondary">Status:</span> Released</li>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(
+              movie.budget
+            )}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(
+              movie.revenue
+            )}</li>
+            <li><span class="text-secondary">Runtime:</span> ${
+              movie.runtime
+            } minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
           </ul>
           <h4>Production Companies</h4>
-          <div class="list-group">Company 1, Company 2, Company 3</div>
+          <div class="list-group">  ${movie.production_companies
+            .map((company) => `<span>${company.name}</span>`)
+            .join(', ')}</div>
         </div>`;
   document.querySelector('#movie-details').appendChild(div);
 }
@@ -148,6 +158,20 @@ function showSpinner() {
 
 function hideSpinner() {
   document.querySelector('.spinner').classList.remove('show');
+}
+
+function addCommasToNumber(num) {
+  let numStr = num.toString(); // convert number to string
+  let numArr = numStr.split('.'); // split the string at decimal point
+  let wholeNum = numArr[0]; // get the whole number part
+  let decimalNum = numArr[1]; // get the decimal part (if any)
+  let regex = /\B(?=(\d{3})+(?!\d))/g; // regex pattern to match every 3 digits
+  wholeNum = wholeNum.replace(regex, ','); // add commas to the whole number
+  if (decimalNum) {
+    return wholeNum + '.' + decimalNum; // if there is a decimal part, add it back
+  } else {
+    return wholeNum; // if there is no decimal part, return only the whole number
+  }
 }
 
 // Init app
