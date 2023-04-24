6bb5afa08db63b8aca886e97ffb3ae1f43f11b37
Radim Vrška <87022843+radim567@users.noreply.github.com>
2023-04-24 19:58:45 +0200

    Slider done

--- js/script.js
+++ js/script.js
@@ -227,9 +227,46 @@ function displayBackgroundImage(type, backdropPath) {
 }
 // Display slider movies
 async function displaySlider() {
-  const results = await fetchApiData('/movie/now_playing');
+  const { results } = await fetchApiData('/movie/now_playing');
 
-  console.log(results);
+  results.forEach((movie) => {
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+      
+          <a href="movie-details.html?id=${movie.id}">
+            <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" 
+           alt="${movie.title}" />
+          </a>
+          <h4 class="swiper-rating">
+            <i class="fas fa-star text-secondary"></i>
+            ${movie.vote_average.toFixed(1)} / 10
+          </h4>`;
+
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  });
+}
+
+// Function initSwiper
+
+function initSwiper() {
+  const swiper = new Swiper('.swiper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      500: { slidesPerView: 2 },
+      768: { slidesPerView: 3 },
+      1024: { slidesPerView: 4 },
+    },
+  });
 }
 
 // Fetch data from TMDB API
